744868464402aef0e6d70574badc8f23a26ed1af
James Weldy <jamesweldy@Jims-MacBook-Pro.local>
2021-11-11 08:27:52 -0600

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +42,4 @@ function App() {
 }
 
 export default App;
+*/
